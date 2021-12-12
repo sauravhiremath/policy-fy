98cb1318d4d29654286d00e0eb036abe60af9ce2
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-12-12 19:38:02 +0530

    feat: multi-level support

--- src/index.ts
+++ src/index.ts
@@ -3,60 +3,17 @@ import { buildYaml } from "./utils";
 import fs from "fs";
 import { parser } from "./parser";
 
-export const basePolicy = {
-  apiVersion: "v1",
-  policies: [
-    {
-      name: "policy1",
-      isDefault: true,
-      rules: {
-        identifier: "CUSTOM_WORKLOAD_INCORRECT_ENVIRONMENT_LABELS",
-        messageOnFailure: "Failed CUSTOM_WORKLOAD_INCORRECT_ENVIRONMENT_LABELS",
-      },
-    },
-  ],
-  customRules: [],
-};
-
-// Get document, or throw exception on error
 try {
   const doc = yaml.load(
     fs.readFileSync(__dirname + "/../k8/sample1.yml", "utf8")
   );
   // console.log(doc);
-  // fs.writeFileSync(util.inspect(doc), "./dump/sample1.dump.json");
-
-  const context = {
-    customRules: [
-      {
-        identifier: "CUSTOM_WORKLOAD_INCORRECT_ENVIRONMENT_LABELS",
-        name: "Name for CUSTOM_WORKLOAD_INCORRECT_ENVIRONMENT_LABELS",
-        defaultMessageOnFailure: "CUSTOM_WORKLOAD_INCORRECT_ENVIRONMENT_LABELS",
-        schema: {
-          properties: {
-            metadata: {
-              properties: {
-                labels: {
-                  properties: {
-                    environment: {
-                      enum: ["prod", "staging", "dev"],
-                    },
-                  },
-                  required: "environment",
-                },
-              },
-              required: "labels",
-            },
-          },
-        },
-      },
-    ],
-  };
+  // fs.writeFileSync(
+  //   __dirname + "/../output/sample1.dump.json",
+  //   JSON.stringify(doc)
+  // );
   const parsedPolicyCtx = parser(doc);
   buildYaml({ ...parsedPolicyCtx }, "sample1-policy-gen.yml");
-  // for (const key in doc) {
-  //   console.log(doc.);
-  // }
 } catch (e) {
   console.log(e);
 }
--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,30 @@
-import { basePolicy } from ".";
 import baselvl from "../schema/index.json";
+import metadataSchema from "../schema/metadata.json";
 const lvl1 = ["apiVersion", "kind", "metadata", "spec"];
-const k8 = {};
-// Any children property below can be passed enum values
-const enumifyMetadata = ["labels", "annotations"];
+
+const METADATA_SCHEMA: any = metadataSchema;
+
+export const getBasePolicy = (ctx: any) => {
+  const identifiers: Array<string> = ctx.customRules.map(
+    ({ identifier }: { identifier: string }) => identifier
+  );
+  return {
+    apiVersion: "v1",
+    policies: [
+      {
+        name: "autogen-pass-sample1",
+        isDefault: true,
+        rules: identifiers.map((v) => ({
+          identifier: v,
+          messageOnFailure: "",
+        })),
+      },
+    ],
+  };
+};
 
 export function parser(doc: any) {
-  const policy: any = { ...basePolicy };
+  const policy: any = { customRules: [] };
   // First lvl parse
   const lvl1Props = baselvl.properties;
   for (const lvl1 in doc) {
@@ -15,8 +33,8 @@ export function parser(doc: any) {
         case "apiVersion": {
           const rule = {
             identifier: `${lvl1.toUpperCase()}_CHECK`,
-            messageOnFailure: `Failed ${lvl1.toUpperCase()}_CHECK`,
-            schema: buildSchema([{ key: lvl1, values: [doc[lvl1]] }]),
+            defaultMessageOnFailure: `Failed ${lvl1.toUpperCase()}_CHECK`,
+            schema: buildSchema({ key: lvl1, values: [doc[lvl1]] }, []),
           };
           policy.customRules.push(rule);
           break;
@@ -24,10 +42,65 @@ export function parser(doc: any) {
         case "kind": {
           const rule = {
             identifier: `${lvl1.toUpperCase()}_CHECK`,
-            messageOnFailure: `Failed ${lvl1.toUpperCase()}_CHECK`,
-            schema: buildSchema([{ key: lvl1, values: [doc[lvl1]] }]),
+            defaultMessageOnFailure: `Failed ${lvl1.toUpperCase()}_CHECK`,
+            schema: buildSchema({ key: lvl1, values: [doc[lvl1]] }, []),
           };
           policy.customRules.push(rule);
+          break;
+        }
+        case "metadata": {
+          const keywords = Object.keys(metadataSchema).flatMap((key) => {
+            if (METADATA_SCHEMA[key].properties) {
+              const cache = [];
+              for (const propKey of Object.keys(
+                METADATA_SCHEMA[key].properties
+              )) {
+                cache.push({
+                  key: propKey,
+                  description:
+                    METADATA_SCHEMA[key].properties[propKey].description ||
+                    null,
+                  type: METADATA_SCHEMA[key].properties[propKey].type || null,
+                });
+              }
+              return cache;
+            }
+          });
+          const metadataProperties = doc[lvl1];
+          const builder = [];
+          for (const prop in metadataProperties) {
+            for (const keyword of keywords.values()) {
+              if (keyword?.key === prop) {
+                if (keyword?.type === "string") {
+                  builder.push({
+                    schemaConfig: {
+                      parents: ["metadata"],
+                      key: prop,
+                      values:
+                        typeof metadataProperties[prop] === "string"
+                          ? [metadataProperties[prop]]
+                          : metadataProperties[prop],
+                    },
+                    identifier: `${lvl1.toUpperCase()}_${prop}_CHECK`,
+                    defaultMessageOnFailure: `Failed ${lvl1.toUpperCase()}_${prop}_CHECK\n${
+                      keyword.description &&
+                      `Description: ${keyword.description}`
+                    }`,
+                  });
+                }
+                // Keeping only the first type of 'property' inside datree policy rules
+                break;
+              }
+            }
+          }
+          for (const buildCtx of builder) {
+            const rule = {
+              identifier: buildCtx.identifier,
+              defaultMessageOnFailure: buildCtx.defaultMessageOnFailure,
+              schema: buildSchema(buildCtx.schemaConfig, [lvl1]),
+            };
+            policy.customRules.push(rule);
+          }
           break;
         }
         default:
@@ -35,23 +108,36 @@ export function parser(doc: any) {
       }
     }
   }
-  return policy;
+  return { ...getBasePolicy(policy), ...policy };
 }
 
 // ctx = [{ key, values }, { key, values }, {}]
 export function buildSchema(
-  ctx: Array<{ key: string; values: Array<string> }>
+  ctx: { key: string; values: Array<string> },
+  parents: Array<string>
 ) {
-  let build: any = {};
-  let currPtr = build;
-  for (const [index, atomicCtx] of ctx.entries()) {
-    currPtr.properties = {
-      [atomicCtx.key]: {},
-    };
-    if (index === ctx.length - 1) {
-      currPtr.properties[atomicCtx.key].enum = [...atomicCtx.values];
+  const buildParentProperties = (
+    parents: Array<string>,
+    build: any,
+    ctx: any
+  ) => {
+    if (parents.length <= 0) {
+      return { properties: ctx };
     }
-    build = currPtr;
-  }
+    for (const parent of parents) {
+      build.properties = {
+        [parent]: buildParentProperties(
+          parents.slice(1),
+          build.properties,
+          ctx
+        ),
+      };
+    }
+    return build;
+  };
+  let build: any = {};
+  build = buildParentProperties(parents, build, {
+    [ctx.key]: { enum: [...ctx.values] },
+  });
   return build;
 }
